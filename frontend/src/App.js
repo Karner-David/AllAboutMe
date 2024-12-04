fb0f1c3802f1ac40992dd8f573ddb60067a06525
Karner David <viEclipsekd@gmail.com>
2024-12-04 16:40:20 -0600

    created react app, will now try to figure out how to make a file cabinet

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Drawer from './components/Drawer/Drawer';
+
+function App() {
+  return (
+    <div className="app">
+      <Drawer/>
+    </div>
+  );
+}
+
+export default App;
