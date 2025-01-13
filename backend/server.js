bf89d23e8121179d31d7b9e02527cbe76d9b2e51
Karner David <viEclipsekd@gmail.com>
2025-01-12 22:08:56 -0600

    pushing upload files to see if it will fix image loading problems

--- backend/server.js
+++ backend/server.js
@@ -20,12 +20,12 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.log('Failed to connect to MongoDB', err));
 
-// Create uploads directory if it doesn't exist
-const uploadsDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadsDir)) {
-    fs.mkdirSync(uploadsDir, { recursive: true });
-    console.log('Uploads directory created');
-}
+// // Create uploads directory if it doesn't exist
+// const uploadsDir = path.join(__dirname, 'uploads');
+// if (!fs.existsSync(uploadsDir)) {
+//     fs.mkdirSync(uploadsDir, { recursive: true });
+//     console.log('Uploads directory created');
+// }
 
 // Serve static files from the uploads folder
 app.use('/uploads', express.static(uploadsDir));
